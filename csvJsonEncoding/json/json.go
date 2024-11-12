ffdb889fe158087db2df589504634027c39a3fbd
sagar0419 <sagar.rajput27@gmail.com>
2024-11-12 17:48:59 +0530

    added json handling  logic

---
+++ csvJsonEncoding/json/json.go
@@ -0,0 +1,74 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Employee struct {
+	Name       string `json:"name"`
+	Age        int    `json:"age"`
+	Department string `json:"dept"`
+}
+
+var newEmployee = `{
+					 "name":"Alice",
+					 "age":30,
+					 "dept":"IT"
+				   }`
+
+func EncodeJson() {
+
+	employee := Employee{
+		Name:       "Alice",
+		Age:        30,
+		Department: "IT",
+	}
+	emp, err := json.Marshal(employee)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(string(emp))
+}
+
+func DecodeJson() {
+
+	var emp Employee
+
+	if err := json.Unmarshal([]byte(newEmployee), &emp); err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("Name: %s, Age: %d, Department: %s\n", emp.Name, emp.Age, emp.Department)
+}
+
+func ReadJson(jsonFile string) {
+
+	file, err := os.ReadFile(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Printf("\nThe content of the JSON file is below.\n\n%s\n", file)
+}
+
+func WriteJson(jsonFile string) {
+	file, err := os.OpenFile(jsonFile, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer file.Close()
+
+	line := "\n" + newEmployee
+	_, err = file.WriteString(line)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("\nThe line %s is added in %s file\n", newEmployee, jsonFile)
+}
--- csvJsonEncoding/main.go
+++ csvJsonEncoding/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	csv "github.com/sagar0419/csvJsonEncoding/csv"
+	json "github.com/sagar0419/csvJsonEncoding/json"
 )
 
 func main() {
-	csv.ReadCsv("timezone.csv")
-	csv.WriteCsv("timezone.csv")
+	// csv.ReadCsv("timezone.csv")
+	// csv.WriteCsv("timezone.csv")
+	json.EncodeJson()
+	json.DecodeJson()
+	json.ReadJson("sample3.json")
+	json.WriteJson("sample3.json")
 }
